56656584fc6ac216b38cb8bf52dc4fa270de5121
Aizpea Babaze <aizpea_babaze@hotmail.com>
2018-02-05 22:54:01 +0100

    create admin migration

---
+++ config/Migrations/20180205214416_AddUser.php
@@ -0,0 +1,26 @@
+<?php
+use Migrations\AbstractMigration;
+use Cake\ORM\TableRegistry;
+class AddUser extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+      $UsersTable = TableRegistry::get('Users');
+      $user = $UsersTable->newEntity();
+
+      $user->name = 'Admin';
+      $user->email = 'admin@admin.com';
+      $user->password= 'default';
+      $user->active= true;
+      $user->role = 'admin';
+
+    $UsersTable->save($user);
+    }
+}
