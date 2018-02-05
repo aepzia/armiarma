4ebbdcca6879d620a9ecfe21430a8adf0aeb6af5
Aizpea Babaze <aizpea_babaze@hotmail.com>
2018-02-05 22:13:35 +0100

    debugkit

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -211,6 +211,7 @@ Type::build('timestamp')
  * Only try to load DebugKit in development mode
  * Debug Kit should not be installed on a production system
  */
+Plugin::loadAll();
 if (Configure::read('debug')) {
     Plugin::load('DebugKit', ['bootstrap' => true]);
 }
