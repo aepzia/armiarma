a5970d7a34e9d8735ca54c9459c44f20e2cda128
Aizpea Babaze <aizpea_babaze@hotmail.com>
2018-03-12 23:40:57 +0100

    email proba

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -3,7 +3,16 @@ namespace App\Controller;
 
 use App\Controller\AppController;
 use Cake\Event\Event;
-use Cake\Mailer\Email;
+
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'yourapp.heroku.com',
+  :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
 
 /**
  * Users Controller
