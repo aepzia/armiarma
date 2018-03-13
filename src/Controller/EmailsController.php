7d9a90d07a47d03c39dae3e649273691e69c6d28
Aizpea Babaze <aizpea_babaze@hotmail.com>
2018-03-13 20:23:00 +0100

    email proba

--- src/Controller/EmailsController.php
+++ src/Controller/EmailsController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 use App\Controller\AppController;
 use Cake\Mailer\Email;
 
-class EmailController extends AppController{
+class EmailsController extends AppController{
 
 //public $components=array('Email');
 
