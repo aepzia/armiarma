37adf443b008d5f10997849385dc7b08f343204a
Aizpea Babaze <aizpea_babaze@hotmail.com>
2018-03-13 14:25:01 +0100

    email proba

--- src/Controller/EmailController.php
+++ src/Controller/EmailController.php
@@ -3,16 +3,19 @@ class EmailController extends AppController{
 
 public $components=array('Email');
 
-function send(){
+  function send(){
 
-//create an array of values to be replaced in email html template//
-$this->Email->to = 'ababaze@gmail.com'; //receiver email id
-$this->Email->subject = 'Subject Line';
-$this->Email->replyTo = 'reply@address.com'; //reply to email//
-$this->Email->template = 'sample';//email template //
-$this->Email->sendAs = 'html';
-if($this->Email->send()){
-//mail send //
+  //create an array of values to be replaced in email html template//
+      $this->Email->to = 'ababaze@gmail.com'; //receiver email id
+      $this->Email->subject = 'Subject Line';
+      $this->Email->replyTo = 'reply@address.com'; //reply to email//
+      $this->Email->template = 'sample';//email template //
+      $this->Email->sendAs = 'html';
+      if($this->Email->send()){
+      //mail send //
+      }
+
+  }
 }
 
-}
+?>
