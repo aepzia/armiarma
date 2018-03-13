70dc33d8e5e6aea0cb0c60fcd4b7933fe18fc93a
Aizpea Babaze <aizpea_babaze@hotmail.com>
2018-03-13 01:15:45 +0100

    email proba

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -145,40 +145,15 @@ class UsersController extends AppController
         if($this->request->is('post')){
           $user = $this->Auth->identify();
           if($user){
-              $to = "ababaze@gmail.com";
-              $subject = "HTML email";
-              $message = "
-              <html>
-              <head>
-              <title>HTML email</title>
-              </head>
-              <body>
-              <p>This email contains HTML Tags!</p>
-              <table>
-              <tr>
-              <th>Firstname</th>
-              <th>Lastname</th>
-              </tr>
-              <tr>
-              <td>John</td>
-              <td>Doe</td>
-              </tr>
-              </table>
-              </body>
-              </html>
-              ";
+            $this->Email->delivery = 'smtp';
+            $this->Email->from = 'Your Name ';
+            $this->Email->to = 'Recipient Name ';
+            $this->set('name', 'Recipient Name');
+            $this->Email->subject = 'This is a subject';
+            $this->Email->template = 'registration';
+            $this->Email->sendAs = 'both';
+            $this->Email->send();
 
-              // Always set content-type when sending HTML email
-              $headers = "MIME-Version: 1.0" . "\r\n";
-              $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-              // More headers
-              $headers .= 'From: <webmaster@example.com>' . "\r\n";
-              $headers .= 'Cc: myboss@example.com' . "\r\n";
-
-              if(!mail($to,$subject,$message,$headers)){
-                $this->Flash->error('Emaila ez da bidali');
-              }
 
             $this->Auth->setUser($user);
             return $this->redirect($this->Auth->redirectUrl());
