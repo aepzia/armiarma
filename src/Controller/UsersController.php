185a7d6ed1c3d050d92b6da752b40ff4d6c4886c
Aizpea Babaze <aizpea_babaze@hotmail.com>
2018-03-13 01:54:35 +0100

    email proba

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -147,7 +147,10 @@ class UsersController extends AppController
           if($user){
             $mensaje = "Línea 1\r\nLínea 2\r\nLínea 3";
             $mensaje = wordwrap($mensaje, 70, "\r\n");
-            mail('ababaze@gmail.com', 'Mi título', $mensaje);
+            if(!mail('ababaze@gmail.com', 'Mi título', $mensaje)){
+              $this->Flash->error('Emailik ez');
+
+            }
 
 
 
