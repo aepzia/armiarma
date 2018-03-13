4dea557fed0e3f2c1bf85ecf3667d8d134602641
Aizpea Babaze <aizpea_babaze@hotmail.com>
2018-03-13 22:45:30 +0100

    email proba

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -144,7 +144,7 @@ if (!Configure::read('App.fullBaseUrl')) {
 
 Cache::setConfig(Configure::consume('Cache'));
 ConnectionManager::setConfig(Configure::consume('Datasources'));
-//Email::setConfigTransport(Configure::consume('EmailTransport'));
+Email::setConfigTransport(Configure::consume('EmailTransport'));
 //Plugin::load('MadalinIgnisca/Sendgrid', ['bootstrap' => true]);
 Email::setConfig(Configure::consume('Email'));
 Log::setConfig(Configure::consume('Log'));
