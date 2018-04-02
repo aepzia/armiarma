68df384b3dc86c360f55f9a77b2189b18426af61
Aizpea Babaze <aizpea_babaze@hotmail.com>
2018-04-02 16:20:12 +0200

    iframe email

--- src/Controller/ReadersController.php
+++ src/Controller/ReadersController.php
@@ -152,7 +152,7 @@ class ReadersController extends AppController
               ->to('ababaze@gmail.com')
               ->subject('boletina')
               ->transport('sendgrid')
-              ->message('<&nbs;iframe title="YouTube video player" width="480" height="390" src="http://armiarma.herokuapp.com/users/login" frameborder="0" allowfullscreen><&nbs;/iframe>');
+              ->message('<&nbs;iframe title="YouTube video player" width="480" height="390" src="http://armiarma.herokuapp.com/users/login" frameborder="0" allowfullscreen><&nbs;/iframe>')
               ->send();
       }
 }
