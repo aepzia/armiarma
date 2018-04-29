1f89a99ee8891bef045a56424b96ccc5029db096
Aizpea Babaze <aizpea_babaze@hotmail.com>
2018-04-29 19:02:09 +0200

    readers active message

--- src/Controller/ReadersController.php
+++ src/Controller/ReadersController.php
@@ -94,7 +94,7 @@ class ReadersController extends AppController
 
                $message = '<p> Zure erabiltzailea gorde da, hemendik aurrera euskararen inguruko ekintzen informazioa jasoko duzu. </p>';
 
-                $message = $message . '<a href="$onclick" type="button">
+                $message = $message . '<a href="'+$onclick+'" type="button">
      Onartu</a>';
                 $email->from(['ababaze@gmail.com' => 'Armiarma'])
                       ->to($reader->email)
