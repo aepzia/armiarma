eefa8a71be97280cd95ade28bca27345fca7dd4d
Aizpea Babaze <aizpea_babaze@hotmail.com>
2018-05-01 16:25:47 +0200

    event edit

--- src/Controller/EventsController.php
+++ src/Controller/EventsController.php
@@ -130,7 +130,7 @@ class EventsController extends AppController
         $event = $this->Events->get($id, [
             'contain' => []
         ]);
-        $date = $event->data->year() + "/" + $event->data->month()  +"/" + $event->data->day();
+        $date = $event->data +"";
 
 
           if ($this->request->is(['patch', 'post', 'put'])) {
