64cd48d5c7c079b4bc40af5ebc2680692626961c
Aizpea Babaze <aizpea_babaze@hotmail.com>
2018-09-05 12:48:17 +0200

    events index

--- src/Controller/EventsController.php
+++ src/Controller/EventsController.php
@@ -48,6 +48,7 @@ class EventsController extends AppController
             ) )));        }if($current_user['role'] == 'user'){
           $events = $this->paginate($this->Events->find('all', array('order'=>array('hasdata ASC') , 'conditions' => array(
             'accepted' => 1,
+            'hasdata >=' => $now,
               'or' => array(
                 'hasdata >=' => $now,
                 'user_id' => $current_user['id'],
