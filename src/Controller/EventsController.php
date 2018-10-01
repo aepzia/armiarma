1e341066fa3d5759a2ed5379998cf0e4eecf4617
Aizpea Babaze <aizpea_babaze@hotmail.com>
2018-10-01 14:26:59 +0200

    ekitaldiak data

--- src/Controller/EventsController.php
+++ src/Controller/EventsController.php
@@ -45,6 +45,7 @@ class EventsController extends AppController
         if ($current_user['role'] == 'admin'){
           $events = $this->paginate($this->Events->find('all', array('order'=>array('hasdata ASC') , 'conditions' => array(
                 'bukdata >=' => $now,
+                'accepted =' => 1
             ) )));        }if($current_user['role'] == 'user'){
           $events = $this->paginate($this->Events->find('all', array('order'=>array('hasdata ASC') , 'conditions' => array(
             'bukdata >=' => $now,
